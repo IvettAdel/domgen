f329198736833b3e499741d7611b235bd098dccd
Zsiga Gergely <zsi-ga>
2023-02-09 20:12:30 +0100

    vaolami

--- src/app.js
+++ src/app.js
@@ -1,22 +1,10 @@
-var szam = "35";
-var szam2 = '27';
+const ul = document.querySelector('#lista');
 
-console.log(Number(szam) + Number(szam2));
+//li elem generálása
 
-var esik = true;
-if (esik) {
-    console.log('esik....')
+var li = document.createElement('li');
 
-    function calcArea(base, height) {
-        return base * height /2
-    }
-}
-console.log(typeof calcArea)
 
-var pista = {
-    nev:'pisti',
-    kor: 28
-};
-console.log(typeof pista)
-var szamok = [];
-console.log(typeof szamok);
+li.textContent = 'szilva';
+ul. append(li);
+
